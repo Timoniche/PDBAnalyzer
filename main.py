2401dfd0a08b8d869cb6f030b37c5cb884ae55fa
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2021-01-14 14:50:11 +0300

    plots, fix simba further

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from HiCExtractor import chr_i_hic_to_txt_adj
 from PDBAnalyzer import analyze_chr_i
 
 
 def main():
-    analyze_chr_i(1, file_name='3DMax_chr2.pdb')
+    # analyze_chr_i(1, file_name='3DMax_chr2.pdb')
+    chr_i_hic_to_txt_adj(filepath='chr/Rao2014-IMR90-MboI-allreps-filtered.500kb.cool', i=5)
 
 
 if __name__ == '__main__':
