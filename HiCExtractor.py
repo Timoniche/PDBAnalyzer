0d2328ccaf1672f3c8eea2da51af4cafaabd1bf5
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2021-01-14 04:32:29 +0300

    cool chr to adj 3dmax format

--- HiCExtractor.py
+++ HiCExtractor.py
@@ -11,3 +11,16 @@ def chr_i_hic(filepath, i):
     return (c.matrix(balance=False)[left_bin_id:right_bin_id, left_bin_id:right_bin_id],
             count_bins_chr_i,
             c.chromsizes[i])
+
+
+def chr_i_hic_to_txt_adj(filepath, i):
+    hic_file = open('gen_adj_hics/chr' + str(i + 1) + '.txt', 'w')
+    c = cooler.Cooler(filepath)
+    bin_size = c.info['bin-size']
+    count_bins_chr_i = math.ceil(c.chromsizes[i] / bin_size)
+    (left_bin_id, right_bin_id) = c.extent('chr' + str(i + 1))
+    mat = c.matrix(balance=False)[left_bin_id:right_bin_id, left_bin_id:right_bin_id]
+    for i in range(count_bins_chr_i):
+        for j in range(count_bins_chr_i):
+            hic_file.write(f'{i + 1} {j + 1} {mat[i][j]}\n')
+    hic_file.close()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from HiCExtractor import chr_i_hic_to_txt_adj
 from PDBAnalyzer import analyze_chr_i
 
 
 def main():
     analyze_chr_i(0)
+    chr_i_hic_to_txt_adj(filepath='chr/Rao2014-IMR90-MboI-allreps-filtered.500kb.cool', i=1)
 
 
 if __name__ == '__main__':
