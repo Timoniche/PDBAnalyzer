e4004f50ae0436fb4201efa2d2404cc78ea9af1a
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2021-01-14 16:14:33 +0300

    chr1 & chr3 simba

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PDBAnalyzer import analyze_chr_i
 def main():
     # analyze_chr_i(1, file_name='3DMax_chr2.pdb')
     # chr_i_hic_to_txt_adj(filepath='chr/Rao2014-IMR90-MboI-allreps-filtered.500kb.cool', i=5)
-    chr_i_hic_to_npy(i=1, filepath='chr/Rao2014-IMR90-MboI-allreps-filtered.500kb.cool')
+    chr_i_hic_to_npy(i=2, filepath='chr/Rao2014-IMR90-MboI-allreps-filtered.500kb.cool')
 
 
 if __name__ == '__main__':
